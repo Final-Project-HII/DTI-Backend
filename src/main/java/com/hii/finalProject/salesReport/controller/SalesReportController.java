f6e705a17dd96180a8f9fc01f94e3b37d2fe4784
iqbalya25 <iqbalya25@gmail.com>
2024-10-03 05:45:16 +0700

    re create backend sales report

--- src/main/java/com/hii/finalProject/salesReport/controller/SalesReportController.java
+++ src/main/java/com/hii/finalProject/salesReport/controller/SalesReportController.java
@@ -1,15 +1,14 @@
 package com.hii.finalProject.salesReport.controller;
 
 import com.hii.finalProject.order.entity.OrderStatus;
+
 import com.hii.finalProject.salesReport.dto.SalesReportDTO;
 import com.hii.finalProject.salesReport.service.SalesReportService;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 
 @RestController
 @RequestMapping("/api/reports")
@@ -27,13 +26,5 @@ public class SalesReportController {
             @RequestParam(defaultValue = "SHIPPED") OrderStatus saleStatus) {
         SalesReportDTO report = salesReportService.generateDailySalesReport(date, saleStatus);
         return ResponseEntity.ok(report);
-    }
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<String> handleDateFormatException(MethodArgumentTypeMismatchException ex) {
-        if (ex.getCause() instanceof DateTimeParseException) {
-            return ResponseEntity.badRequest().body("Invalid date format. Please use ISO date format (YYYY-MM-DD).");
-        }
-        return ResponseEntity.badRequest().body("Invalid argument: " + ex.getMessage());
     }
 }
