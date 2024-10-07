91b3e33178653ecf3940fe41afc3ed8812f0f022
Hendry Tjahaja <80196576+akaslayer@users.noreply.github.com>
2024-10-07 20:29:04 +0700

    fix(Warehouse):Fix the nearest warehouse function (#46)
    * Feat(Courier):Feature for courier is done
    
    * Feat(Admin Management):Feature for create admin, toogle active admin, edit admin, and get all admin is done
    
    * Feat(Shipping):Feature for shipping is done for fetching and save the rajaOngkir data
    
    * Feat(Logout):Feature for logout is done
    
    * fix(Warehouse):Fix the nearest warehouse function

--- src/main/java/com/hii/finalProject/users/service/UserService.java
+++ src/main/java/com/hii/finalProject/users/service/UserService.java
@@ -4,6 +4,7 @@ package com.hii.finalProject.users.service;
 import com.hii.finalProject.users.dto.*;
 import com.hii.finalProject.users.entity.User;
 import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 
@@ -22,8 +23,10 @@ public interface UserService {
     void newVerificationLink(String email);
     void newResetPasswordLink(String email);
     String sendResetPasswordLink(String email);
+    void changeEmail(String email,ChangeEmailRequestDTO changeEmailRequestDTO);
     Boolean checkResetPasswordLinkIsValid(CheckResetPasswordLinkDTO data);
     ProfileResponseDTO updateProfile(String email, ProfileRequestDTO profileRequestDTO);
+    ProfileResponseDTO updateAvatar(String email, MultipartFile avatar);
     ProfileResponseDTO getProfileData(String email);
     Page<UserResponseDTO> getAllUser(String email, String role, int page, int size);
     void toggleActiveUser(Long id);
