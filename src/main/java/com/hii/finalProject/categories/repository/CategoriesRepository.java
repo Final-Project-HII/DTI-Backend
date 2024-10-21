e6d46b6ce398181a186111bab058859ab1102e6c
001indah <163018968+001indah@users.noreply.github.com>
2024-10-21 10:19:35 +0700

    Update CategoriesRepository.java

--- src/main/java/com/hii/finalProject/categories/repository/CategoriesRepository.java
+++ src/main/java/com/hii/finalProject/categories/repository/CategoriesRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CategoriesRepository extends JpaRepository<Categories, Long>, JpaSpecificationExecutor<Categories> {
     boolean existsByNameIgnoreCase(String name);
     Optional<Categories> findByIdAndDeletedAtIsNull(Long id);
     List<Categories> findAllByDeletedAtIsNull();
     Optional<Categories> findByNameIgnoreCase(String name);
-    
 }
